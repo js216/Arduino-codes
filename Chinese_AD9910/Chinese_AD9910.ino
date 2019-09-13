78c362d6b02dfbe381e22eb41987bf9aa6ca19d0
Jakob Kastelic <jakob.kastelic@yale.edu>
2019-09-13 18:37:06 -0400

    new features

--- Chinese_AD9910/Chinese_AD9910.ino
+++ Chinese_AD9910/Chinese_AD9910.ino
@@ -12,10 +12,39 @@ const int reset_pin = 35;
 
 unsigned int result[8];
 
+void printBits(byte myByte){
+ for(byte mask = 0x80; mask; mask >>= 1){
+   if(mask  & myByte)
+       Serial.print('1');
+   else
+       Serial.print('0');
+ }
+}
+
+char demandSerialByte(const int N = 1000)
+{
+  for (int i=0; i<N; ++i) {
+    if (Serial.available() > 0)
+      return Serial.read();
+    else
+      delay(1);
+  }
+
+  return 0;
+}
+
 void setup()
 {
+  // for AD9910 communication
   SPI.begin();
+
+  // for computer communication
   Serial.begin(9600);
+
+  // for FPGA communication
+  Serial1.setTX(1);
+  Serial1.setRX(0);
+  Serial1.begin(115200, SERIAL_8N1);
 
   // CS is active-low
   pinMode(CS_pin, OUTPUT);
@@ -33,9 +62,49 @@ void serialEvent()
   while (Serial.available() > 0) {
     char cmd = Serial.read();
 
-    switch (cmd) { 
+    switch (cmd) {
+
+      ///////////////////////////////////////////////////////////
+      /////////             COMMANDS FOR FPGA             ///////
+      ///////////////////////////////////////////////////////////
+      
+      // "get" commands
+      case 't':
+      case 'f':
+      case 'n':
+      case 'p':
+      case 'i':
+      case 'd':
+      case 's':
+      case 'l':
+      case 'a':
+      case 'e':
+      case 'o':
+        Serial1.print(cmd);
+        break;
+        
+      // "set" commands
+      case 'T':
+      case 'F':
+      case 'N':
+        Serial1.print(cmd);
+        Serial1.print( demandSerialByte() );
+        break;
+      case 'P':
+      case 'I':
+      case 'S':
+      case 'L':
+        Serial1.print(cmd);
+        Serial1.print( demandSerialByte() );
+        Serial1.print( demandSerialByte() );
+        break;
+        
+      ///////////////////////////////////////////////////////////
+      /////////             COMMANDS FOR AD9910           ///////
+      ///////////////////////////////////////////////////////////
+      
       case '1':
-        read_one_register(0x02);
+        read_one_register( demandSerialByte() );
         break;
         
       case 'r':
@@ -57,6 +126,15 @@ void serialEvent()
         write_register(reg, data);
         break;
     }
+  }
+}
+
+// echo FPGA serial communications to USB
+void serialEvent1()
+{
+  while (Serial1.available() > 0) {
+    char data = Serial1.read();
+    Serial.println(data, HEX);
   }
 }
 
@@ -185,17 +263,4 @@ void write_register(char reg, char data[])
     Serial.print(",");
   }
   Serial.print("\n");
-}
-
-/* ============================
- * AUXILIARY FUNCTIONS
-   ============================ */
-
-void printBits(byte myByte){
- for(byte mask = 0x80; mask; mask >>= 1){
-   if(mask  & myByte)
-       Serial.print('1');
-   else
-       Serial.print('0');
- }
 }
