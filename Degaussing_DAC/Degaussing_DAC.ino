717f98382ec7fe1edffc1a332cd313b6e30bd273
Jakob Kastelic <jakob.kastelic@yale.edu>
2020-09-25 10:49:08 -0400

    latest codes

--- Degaussing_DAC/Degaussing_DAC.ino
+++ Degaussing_DAC/Degaussing_DAC.ino
@@ -1,17 +1,20 @@
 #include <SPI.h>
 
 // pin configuration
-const int CSB = 2;
+const int CSB = 10;
 
 // signal generation parameters
 bool trig = false;
-float freq = 10;
+int di=5, dt=0;
 float t_rise = 1;
-float t_decay = .5;
+float t_decay = 5;
+float amp_f = 0.1;
+int pulse_mode = 0;
+bool step_forward = false;
 
 // internal parameters
 const unsigned int amp_max = 65535;
-unsigned int amp, dt, inc;
+int amp, inc;
 float div_f;
 
 void setup() {
@@ -23,7 +26,7 @@ void setup() {
   digitalWrite(CSB, HIGH);
 
   // configure the DAC
-  set_gain(LOW);
+  set_DAC();
 }
 
 void serialEvent() {
@@ -34,11 +37,36 @@ void serialEvent() {
     // decide what to do with it
     switch (c) {
       case '?':
-        Serial.println("Degaussing DAC board ready.");
+        Serial.println("Degaussing DAC board v2.0 ready.");
+        break;
+
+      case '0':
+        trig = false;
+        amp = 0;
+        break;
+
+      case 's':
+        step_forward = true;
+        break;
+
+      case 'm':
+        pulse_mode = Serial.parseInt();
         break;
 
       case 'f':
-        freq = Serial.parseFloat();
+        di = Serial.parseInt();
+        break;
+
+      case 'F':
+        dt = Serial.parseFloat();
+        break;
+
+      case 'a':
+        amp_f = Serial.parseFloat();
+        break;
+
+      case 'w':
+        write_SPI(Serial.parseInt());
         break;
 
       case 'r':
@@ -48,45 +76,74 @@ void serialEvent() {
       case 'd':
         t_decay = Serial.parseFloat();
         break;
+
+      case 'R':
+        set_DAC();
+        break;
+
+      case 'c':
+        readback_control();
+        break;
         
       case 't':
-        dt = 1000 / 2 / freq;
+        float freq = di * 4.44;
         inc = amp_max / freq / t_rise;
         div_f = exp(1./(freq * t_decay));
         trig = true;
-        break;
-
-      case '0':
-        trig = false;
-        amp = 0;
+        if (pulse_mode == 2)
+          amp = amp_max;
         break;
     }
   }
 }
 
 void loop() {
-  // calculate amplitude
-  if (trig && (amp < amp_max-inc))
-    amp += inc;
-  else {
-    trig = false;
-    amp /= div_f;
-  }
+  // EXPONENTIAL DECAY MODE
+  if (pulse_mode == 0) {
+    // calculate amplitude
+    if (trig && (amp < amp_max-inc))
+      amp += inc;
+    else {
+      trig = false;
+      amp /= div_f;
+    }
+  
+    // write one cycle
+    for (int i=0; i<amp_max; i+=di)
+      write_SPI( amp_f*amp*sin(2*3.14159*i/amp_max)/2 + amp_max/2 );
 
-  // write one cycle
-  delay(dt);
-  write_SPI(amp);
-  delay(dt);
-  write_SPI(0);
+  // SINGLE-PULSE MODE
+  } else if (pulse_mode == 1) {
+    if (trig) {
+      for (int i=0; i<amp_max; i+=di)
+        write_SPI( amp_f*amp_max*sin(2*3.14159*i/amp_max)/2 + amp_max/2 );
+        trig = false;
+    }
+
+  // STEPPED EXP DECAY MODE
+  } else if (pulse_mode == 2) {
+    if (step_forward) {
+      amp /= div_f;
+      for (int i=0; i<amp_max; i+=di) {
+        write_SPI( amp_f*amp*sin(2*3.14159*i/amp_max)/2 + amp_max/2 );
+        delayMicroseconds(dt);
+      }
+      step_forward = false;
+    }
+  }
 }
 
-void set_gain(const bool gain)
+void set_DAC()
 {
   // compose data packet
   byte data[3];
-  data[0] = 0x40;
-  data[1] = gain << 7;
-  data[2] = 0x00;
+  data[0] = B0100;     // write to control register
+  data[1] = 0;         // 5% overrange disabled
+  data[1] |= B00 << 1; // CLEAR selects zero scale
+  data[2] = B000;      // output range = −10 V to +10 V
+  data[2] |= B00 << 3; // power-up voltage = zero scale
+  data[2] |= B0 << 5;  // disable thermal shutdown
+  data[2] |= B0 << 6;  // DAC input for bipolar output range is straight binary coded
 
   // begin transaction
   SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE1));
@@ -105,9 +162,9 @@ void write_SPI(const unsigned int val)
 {
   // compose data packet
   byte data[3];
-  data[0] = 020 | ((val >> 12) & 0xff);
-  data[1] = (val >> 4) & 0xff;
-  data[2] = (val << 4) & 0xff;
+  data[0] = B11;
+  data[1] = (val >> 8) & 0xff;
+  data[2] = val & 0xff;
 
   // begin transaction
   SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE1));
@@ -119,4 +176,31 @@ void write_SPI(const unsigned int val)
   // end transaction
   digitalWrite(CSB, HIGH);
   SPI.endTransaction();
+}
+
+void readback_control()
+{
+  // compose data packet
+  byte data[3];
+  data[0] = B1100;     // read from control register
+  data[1] = 0;
+  data[2] = 0;
+
+  // begin transaction
+  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE1));
+  digitalWrite(CSB, LOW);
+
+  // transfer data
+  for (int i=0; i<3; i++)
+    SPI.transfer(data[i]);
+
+  // end transaction
+  digitalWrite(CSB, HIGH);
+  SPI.endTransaction();
+
+  // interpret the received data
+  if ((data[1] >> 2) & 1)
+    Serial.println("Brownout condition detected.");
+  if ((data[1] >> 3) & 1)
+    Serial.println("Short-circuit condition detected.");
 }
---
+++ Laser_PID_DDS_uC/Laser_PID_DDS_uC.ino
@@ -0,0 +1,469 @@
+#include <SPI.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+///////////////////////////////////////////////
+////    PIN ASSIGNMENT FOR AD9910   ///////////
+///////////////////////////////////////////////
+
+// SPI pins
+const int CS_pin = 36;
+const int IO_update_pin = 38;
+
+// reset pins
+const int reset_pin = 22;
+const int powerdown_pin = 21;
+
+// profiles pins
+const int profile_pins[] = {14, 15, 16};
+ 
+// parallel port pins
+const int D_pins[] = {30,29,28,27,26,25,24,10,9,8,7,6,5,4,3,2};
+const int F_pins[] = {32, 31};
+const int PD = 18;
+const int TEN = 17;
+
+// digital ramp control pins
+const int DRCTL = 33;
+const int DRHOLD = 35;
+const int DROVER = 34;
+
+// other AD9910 pins
+const int RSO = 19; // RAM_SWP_OVR
+const int OSK = 37;
+const int PLL = 20;
+const int SYC = 39;
+
+// external input pin
+const int interruptPin = 23;
+
+///////////////////////////////////////////////
+////    OTHER VARIABLES             ///////////
+///////////////////////////////////////////////
+
+// length of AD9910 internal registers
+const int reg_len[] = {4,4,4,4,4,6,6,4,2,4,4,8,8,4,8,8,8,8,8,8,8,8,4};
+
+// for reading SPI data into
+char result[8];
+
+// configuring the ramp
+volatile bool digital_ramp_enable = false;
+int ramp_time_step_us = 100;
+int ramp_delay_ms = 10;
+
+///////////////////////////////////////////////
+////    DEVICE INITIALIZATION       ///////////
+///////////////////////////////////////////////
+
+void setup()
+{
+  // for AD9910 communication
+  SPI.begin();
+
+  // for computer communication
+  Serial.begin(9600);
+
+  // CS is active-low
+  pinMode(CS_pin, OUTPUT);
+  digitalWrite(CS_pin, LOW);
+
+  pinMode(IO_update_pin, OUTPUT);
+  digitalWrite(IO_update_pin, LOW);
+
+  pinMode(reset_pin, OUTPUT);
+  digitalWrite(reset_pin, LOW);
+
+  pinMode(powerdown_pin, OUTPUT);
+  digitalWrite(powerdown_pin, LOW);
+
+  pinMode(TEN, OUTPUT);
+  digitalWrite(TEN, LOW);
+
+  pinMode(PLL, INPUT);
+
+  for (int i=0; i<2; i++) {
+    pinMode(F_pins[i], OUTPUT);
+    digitalWrite(F_pins[i], LOW);
+  }
+
+  for (int i=0; i<16; i++) {
+    pinMode(D_pins[i], OUTPUT);
+    digitalWrite(D_pins[i], LOW);
+  }
+
+  for (int i=0; i<3; i++)
+    pinMode(profile_pins[i], OUTPUT);
+
+  pinMode(DRCTL, OUTPUT);
+  pinMode(DRHOLD, OUTPUT);
+  pinMode(DROVER, INPUT);
+
+  set_PLL(true);
+  set_profile(0, 85000000, 1000000000, 100);
+}
+
+///////////////////////////////////////////////
+////    SERIAL COMMUNICATION        ///////////
+///////////////////////////////////////////////
+
+void serialEvent()
+{
+  while (Serial.available() > 0) {
+    char cmd = Serial.read();
+
+    switch (cmd) {
+      
+      // REGISTER-LEVEL AD9910 FUNCTIONS
+      
+      case '1':
+        read_one_register( Serial.parseInt() );
+        break;
+
+      case '0':
+        digitalWrite(reset_pin, HIGH);
+        delay(100);
+        digitalWrite(reset_pin, LOW);
+
+        set_profile(0, 85000000, 1000000000, 100);
+        set_PLL(true);
+        Serial.print("Device reset complete.\n");
+        break;
+
+      case 'w':
+        {
+          int reg = Serial.parseInt();
+          char data[reg_len[reg]];
+          for (int i=0; i<reg_len[reg]; i++)
+             data[i] = Serial.read();
+          write_register(reg, data);
+        }
+        break;
+
+      // HIGH-LEVEL AD9910 FUNCTIONS
+
+      case 'a':
+        activate_profile(Serial.parseInt());
+        break;
+      
+      case 'p':
+        set_profile(Serial.parseInt(), Serial.parseInt(), Serial.parseInt(), Serial.parseInt());
+        break;
+
+      case 'o':
+        activate_profile(0);
+        set_profile(0, Serial.parseInt()*1000000, 1000000000, 100);
+        break;
+
+      case 'P':
+        enable_parallel_port(Serial.parseInt());
+        break;
+
+      case 'm':
+        parallel_mode(Serial.parseInt());
+        break;
+
+      case 'A':
+        enable_ASF(Serial.parseInt());
+        break;
+
+      case 'r':
+        enable_refclk_divider(Serial.parseInt());
+        break; 
+
+      case 'N':
+        set_PLL(Serial.parseInt());
+        break;
+
+      case 'n':
+        if (read_PLL())
+          Serial.println("PLL active.");
+        else
+          Serial.println("PLL not active.");
+        break;
+
+      // PID FUNCTIONS
+
+      case 'd':
+        write_parallel(Serial.parseInt(), 16);
+        break;
+    }
+  }
+}
+
+///////////////////////////////////////////////
+////    MAIN DEVICE LOOP            ///////////
+///////////////////////////////////////////////
+
+void loop()
+{
+}
+
+///////////////////////////////////////////////
+////    AD9910 SPI COMMUNICATION    ///////////
+///////////////////////////////////////////////
+
+void read_register(int reg)
+{
+  // register sanity check
+  if (reg >= 23) {
+    Serial.print("Invalid register: ");
+    Serial.print(reg, HEX);
+    Serial.print("\n");
+    return;
+  }
+  
+  // begin transaction
+  SPI.beginTransaction(SPISettings(10000, MSBFIRST, SPI_MODE0));
+
+  // flash the IO_RESET (as well as the CS) to clear potential unfinished transactions
+  digitalWrite(CS_pin, HIGH);
+  delay(5);
+  digitalWrite(CS_pin, LOW);
+  
+  SPI.transfer(reg | B10000000);
+
+  // read data
+  for (int i=0; i<reg_len[reg]; i++)
+    result[i] = SPI.transfer(0x00);
+
+  // end transaction
+  SPI.endTransaction();
+}
+
+void read_one_register(int reg)
+{
+  // Read out the contents of the register
+  Serial.print("Reading from register ");
+  Serial.print(reg, HEX);
+  Serial.print(":   \n  ");
+  read_register(reg);
+  for (int i=0; i<reg_len[reg]; i++) {
+    printBits(result[i]);
+    Serial.print(",");
+  }
+  Serial.print("\n");
+}
+
+void write_register(int reg, char data[])
+{
+  // begin transaction
+  SPI.beginTransaction(SPISettings(10000, MSBFIRST, SPI_MODE0));
+
+  // flash the IO_RESET (as well as the CS) to clear potential unfinished transactions
+  digitalWrite(CS_pin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(CS_pin, LOW);
+
+  // transfer data
+  SPI.transfer(reg & B01111111);
+  for (int i=0; i<reg_len[reg]; i++) {
+    // do the SPI transfer
+    result[i] = SPI.transfer(data[i]);
+    
+    // transfer data from IO buffers to internal registers
+    digitalWrite(IO_update_pin, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(IO_update_pin, LOW);
+  }
+
+  // end transaction
+  SPI.endTransaction();
+}
+
+///////////////////////////////////////////////
+////    AD9910 CONFIGURATION        ///////////
+///////////////////////////////////////////////
+
+void activate_profile(const int profile)
+{
+  for (int i=0; i<3; i++) {
+    if ((profile>>i) & 1)
+      digitalWrite(profile_pins[i], HIGH);
+    else
+      digitalWrite(profile_pins[i], LOW);
+  }
+}
+
+void set_profile(const int profile, const int Fout, const int Fsysclk, const int ASF_percent)
+{
+  // turn off PLL
+  set_PLL(false);
+  
+  // find the profile register
+  const int profile_reg = profile + 14;
+  
+  // calculate the frequency tuning word
+  int FTW = (pow(2,32) * Fout) / Fsysclk;
+  unsigned char FTW_bytes[4];
+  for (int i=0; i<4; i++)  
+    FTW_bytes[i] = (FTW >> (3-i)*8) & 0xFF;
+
+  // calculate amplitude scale factor
+  int ASF = int(ASF_percent * (pow(2,14)-1) / 100);
+  unsigned char ASF_bytes[4];
+  for (int i=0; i<2; i++)
+    ASF_bytes[i] = (ASF >> (1-i)*8) & 0xFF;
+
+  // write to the profile register
+  char data[reg_len[profile_reg]];
+  for (int i=0; i<2; i++)
+    data[i] = ASF_bytes[i];
+  for (int i=2; i<4; i++)
+    data[i] = 0;
+  for (int i=4; i<8; i++)
+    data[i] = FTW_bytes[i-4];
+  write_register(profile_reg, data);
+
+  // turn PLL back on
+  set_PLL(true);
+}
+
+void enable_parallel_port(bool ENABLE)
+{
+  // read register 0x01
+  char data[reg_len[0x01]];
+  read_register(0x01);
+  for (int i=0; i<8; i++)
+    data[i] = result[i];
+
+  // flip the "Parallel data port enable" bit
+  if (ENABLE == true) {
+    digitalWrite(TEN, HIGH);
+    data[3] |= B00010000;
+  } else {
+    digitalWrite(TEN, LOW);
+    data[3] &= B11101111;
+  }
+
+  // write the new register values
+  write_register(0x01, data);
+}
+
+void enable_ASF(bool ENABLE)
+{
+  // read register 0x01
+  char data[reg_len[0x01]];
+  read_register(0x01);
+  for (int i=0; i<8; i++)
+    data[i] = result[i];
+
+  // flip the "Enable amplitude scale from single ton profiles" bit
+  if (ENABLE == true) {
+    data[0] |= B00000001;
+  } else {
+    data[0] &= B11111110;
+  }
+
+  // write the new register values
+  write_register(0x01, data);
+}
+
+void enable_refclk_divider(bool ENABLE)
+{
+  // read register 0x02
+  char data[reg_len[0x02]];
+  read_register(0x02);
+  for (int i=0; i<8; i++)
+    data[i] = result[i];
+
+  // flip the "REFCLK input divider bypass" bit
+  if (ENABLE == true) {
+    data[2] &= B01111111;
+  } else {
+    data[2] |= B10000000;
+  }
+
+  // write the new register values
+  write_register(0x02, data);
+}
+
+void parallel_mode(int mode)
+{
+  if (mode == 0) { // amplitude modulation
+    digitalWrite(F_pins[1], LOW);
+    digitalWrite(F_pins[0], LOW);
+  } else if (mode == 1) { // phase modulation
+    digitalWrite(F_pins[1], LOW);
+    digitalWrite(F_pins[0], HIGH);
+  } else if (mode == 2) { // frequency modulation
+    digitalWrite(F_pins[1], HIGH);
+    digitalWrite(F_pins[0], LOW);
+  } else if (mode == 3) { // amplitude & phase modulation
+    digitalWrite(F_pins[1], HIGH);
+    digitalWrite(F_pins[0], HIGH);
+  } else
+    Serial.println("ERROR: Invalid mode.");
+}
+
+void set_PLL(bool ENABLE)
+{
+  // read register 0x02
+  char data[reg_len[0x02]];
+  read_register(0x02);
+  for (int i=0; i<reg_len[0x02]; i++)
+    data[i] = result[i];
+
+  if (ENABLE) {
+    // select VCO_SEL = VCO5 (for operation at 920-1030 MHz)
+    data[0] &= B11111000;
+    data[0] |= B00000101;
+
+    // write N=25 value (for 40MHz * 25 = 1 GHz clock)
+    data[3] = 25 << 1;
+
+    // set the "PLL enable" bit
+    data[2] |= B00000001;
+    
+  } else {
+    data[2] &= B11111110; // clear the "PLL enable" bit
+    data[0] |= B00000111; // set VCO SEL bits to "PLL bypass"
+    data[3] = 0;          // set N = 0
+  }
+
+  // write the new register values
+  write_register(0x02, data);
+}
+
+bool read_PLL()
+{
+  if (digitalRead(PLL))
+    return true;
+  else
+    return false;
+}
+
+///////////////////////////////////////////////
+////    CONVENIENCE FUNCTIONS       ///////////
+///////////////////////////////////////////////
+
+void printBits(byte myByte){
+ for(byte mask = 0x80; mask; mask >>= 1){
+   if(mask & myByte)
+       Serial.print('1');
+   else
+       Serial.print('0');
+ }
+}
+
+char demandSerialByte(const int N = 1000)
+{
+  for (int i=0; i<N; ++i) {
+    if (Serial.available() > 0)
+      return Serial.read();
+    else
+      delay(1);
+  }
+
+  return 0;
+}
+
+void write_parallel(const int n, const int nbits)
+{
+  for (int i=0; i<nbits; i++) {
+    if ((n>>i)&1)
+      digitalWrite(D_pins[i+16-nbits], HIGH);
+    else
+      digitalWrite(D_pins[i+16-nbits], LOW);
+  }
+}
