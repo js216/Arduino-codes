04bcc803c2e096defd05568a8aa957d418827d88
Jakob Kastelic <jakob.kastelic@yale.edu>
2022-03-17 11:58:59 -0500

    latest changes

--- ShimCoils_v1_3/ShimCoils_v1_3.ino
+++ ShimCoils_v1_3/ShimCoils_v1_3.ino
@@ -23,7 +23,7 @@ void serialEvent() {
     // decide what to do with it
     switch (c) {
       case '?':
-        Serial.write("Shim coils v1.3a, board A, ready.\n");
+        Serial.write("Shim coils v1.3 ready.\n");
         break;
 
       case 's':
@@ -45,39 +45,20 @@ void loop() {
 /////////////////////////////////
 
 
-void set_DAC(unsigned int ch, const unsigned int val)
+void set_DAC(const unsigned int ch, const unsigned int val)
 {
-  // decide which DAC to address
-  int invert_CS;
-  if (ch < 8)
-    invert_CS = 1;
-  else {
-    invert_CS = 0;
-    ch -= 8;
-  }
-  
   // write and update DAC register ch
   DAC_data[0] = (B0011 << 4) + ch;
   DAC_data[1] = (val >> 8);
   DAC_data[2] = val;
-  DAC_SPI(invert_CS);
+  DAC_SPI();
 }
 
-void DAC_SPI(int invert_CS)
+void DAC_SPI()
 {
   SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE2));
-
-  if (invert_CS)
-    digitalWrite(CS_DAC, HIGH);
-  else
-    digitalWrite(CS_DAC, LOW);
-    
+  digitalWrite(CS_DAC, LOW);
   SPI.transfer(DAC_data, 3);
-
-  if (invert_CS)
-    digitalWrite(CS_DAC, LOW);
-  else
-    digitalWrite(CS_DAC, HIGH);
-
+  digitalWrite(CS_DAC, HIGH);
   SPI.endTransaction();
 }
