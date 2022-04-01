46df53ff97c43fb8f6eebefc25beab763e7bca3a
Jakob Kastelic <jakob.kastelic@yale.edu>
2022-04-01 10:53:30 -0500

    update degaussing relay board to Arduino Nano Every

--- Degaussing_relay_board/Degaussing_relay_board.ino
+++ Degaussing_relay_board/Degaussing_relay_board.ino
@@ -1,69 +1,48 @@
-const int relay_delay = 10;
-
-const int R1=2, R2=3, R3=4, R4=5, R5=6;
+const int R[] {2, 3, 4, 5, 6};
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(R1, OUTPUT);
-  pinMode(R2, OUTPUT);
-  pinMode(R3, OUTPUT);
-  pinMode(R4, OUTPUT);
-  pinMode(R5, OUTPUT);
+  for (int i=0; i<5; i++)
+    pinMode(R[i], OUTPUT);
 
-  clear_all();
+  disable_all();
 }
 
-void serialEvent() {
-  while (Serial.available()) {
+void disable_all()
+{
+  for (int i=0; i<5; i++)
+    digitalWrite(R[i], LOW);
+}
+
+void enable_all()
+{
+  for (int i=0; i<5; i++)
+    digitalWrite(R[i], HIGH);
+}
+
+void loop() {
+    while (Serial.available()) {
     // read user input
     char c = (char)Serial.read();
 
     // decide what to do with it
     switch (c) {
       case '0':
-        clear_all();
+        disable_all();
         break;
       case '1':
-        clear_all();
-        delay(relay_delay);
-        digitalWrite(R5, HIGH);
+        enable_all();
         break;
-      case '2':
-        clear_all();
-        delay(relay_delay);
-        digitalWrite(R5, HIGH);
+      case 'e':
+        digitalWrite(R[Serial.parseInt()-1], HIGH);
         break;
-      case '3':
-        clear_all();
-        delay(relay_delay);
-        digitalWrite(R5, HIGH);
-        break;
-      case '4':
-        clear_all();
-        delay(relay_delay);
-        digitalWrite(R5, HIGH);
-        break;
-      case '5':
-        clear_all();
-        delay(relay_delay);
-        digitalWrite(R5, HIGH);
+      case 'd':
+        digitalWrite(R[Serial.parseInt()-1], LOW);
         break;
       case '?':
         Serial.write("Degaussing relay board v1.3 ready.\n");
         break;
     }
   }
-}
-
-void clear_all()
-{
-  digitalWrite(R1, LOW);
-  digitalWrite(R2, LOW);
-  digitalWrite(R3, LOW);
-  digitalWrite(R4, LOW);
-  digitalWrite(R5, LOW);
-}
-
-void loop() {
 }
