afe67ea548e2120d552a65557633dce262f28c0c
Jakob Kastelic <jakob.kastelic@yale.edu>
2022-06-13 18:04:55 -0500

    only print out unique samples

--- DecaPSU_to_ADC/DecaPSU_to_ADC.ino
+++ DecaPSU_to_ADC/DecaPSU_to_ADC.ino
@@ -1,14 +1,22 @@
 #include <SPI.h>
 
 // pin configuration
+const int sens_to_ch[] = {1, 2, 3, 13, 14, 0, 10, 12, 11, 8, 7, 9, 4, 6, 5};
+const int ch_to_sens[] = {5, 0, 1, 2, 12, 14, 13, 10, 9, 11, 6, 8, 7, 3, 4};
 const int CS_ADC    = 10;
 const int MISO_pin  = 12;
+const int MISO_pin2 = 9;
 const int MOSI_pin  = 11;
 const int SCK_pin   = 13;
 
 // state variables
 byte ADC_data[5];
 int readings[15];
+bool readings_rdy[15];
+bool print_enable[15];
+int offs[15];
+int dt = -1;
+elapsedMillis last_printed, t0;
 
 void setup() {
   Serial.begin(9600);
@@ -19,15 +27,13 @@ void setup() {
 
   // pin configuration
   pinMode(CS_ADC, OUTPUT);
+  pinMode(MISO_pin2, INPUT);
   digitalWrite(CS_ADC, HIGH);
 
-  // enable all channels
-  for (int i=0; i<16; i++)
-    set_ADC_ch(i,1);
-
+  disable_all_sensors();
   set_ADC_setup();
-  set_ADC_filter();
   set_ADC_mode();
+  set_ADC_filter(100);
   set_ADC_interface();
 }
 
@@ -44,67 +50,62 @@ void serialEvent() {
         Serial.write(") v2.0 ready.\n");
         break;
 
-      case 's':
-        set_ADC_ch(Serial.parseInt(), Serial.parseInt());
+      case 'd':
+        disable_all_sensors();
         break;
 
-      case 'r':
-        print_ADC_data();
+      case 's':
+        set_ADC_ch(sens_to_ch[Serial.parseInt()], Serial.parseInt());
+        break;
+
+      case 'o':
+        for (int i=0; i<15; i++)
+          offs[i] = readings[i];
+        break;
+
+      case 'f':
+        set_ADC_filter(Serial.parseInt());
+        break;
+
+      case 'p':
+        dt = Serial.parseInt();
         break;
     }
   }
 }
 
 void loop() {
-  for (int i=0; i<10; i++) {
-    readout_ADC();
-    delay(1);
-  }
+  readout_ADC();
 
-  Serial.print(get_ADC_voltage(13));
-  Serial.write(", ");
-  Serial.print(get_ADC_voltage(14));
-  Serial.write(", ");
-  Serial.print(get_ADC_voltage(0));
-  Serial.write(", ");
-  
-  Serial.print(get_ADC_voltage(1));
-  Serial.write(", ");
-  Serial.print(get_ADC_voltage(2));
-  Serial.write(", ");
-  Serial.print(get_ADC_voltage(3));
-  Serial.write(", ");
-    
-  Serial.print(get_ADC_voltage(10));
-  Serial.write(", ");
-  Serial.print(get_ADC_voltage(12));
-  Serial.write(", ");
-  Serial.print(get_ADC_voltage(11));
-  Serial.write(", ");
+  if (dt >= 0) {
+    if (last_printed >= dt) {
+      last_printed -= dt;
+
+      // check all required values are ready
+      for (int i=0; i<15; i++)
+        if ((print_enable[i]) && (!readings_rdy[i]))
+          return;
       
-  Serial.print(get_ADC_voltage(8));
-  Serial.write(", ");
-  Serial.print(get_ADC_voltage(7));
-  Serial.write(", ");
-  Serial.print(get_ADC_voltage(9));
-  Serial.write(", ");
-  
-  Serial.write("\n");
+      // printout the values
+      Serial.print(t0);
+      for (int i=0; i<15; i++)
+        if (print_enable[i]) {
+          Serial.write(", ");
+          Serial.print(readings[i] - offs[i]);
+          readings_rdy[i] = false;
+        }
+      Serial.write("\n");
+    }
+  } else {
+    t0 = 0;
+    last_printed = 0;
+  }
 }
 
 
 /////////////////////////////////
 /// ADC FUNTIONS ///////////////
 /////////////////////////////////
-
-void write_ADC(unsigned int len)
-{
-  SPI.beginTransaction(SPISettings(100000, MSBFIRST, SPI_MODE0));
-  digitalWrite(CS_ADC, LOW);
-  SPI.transfer(ADC_data, len);
-  digitalWrite(CS_ADC, HIGH);
-  SPI.endTransaction();
-}
 
 void print_ADC_ID()
 {
@@ -118,7 +119,7 @@ void print_ADC_ID()
   Serial.print('X');
 }
 
-void print_ADC_register(byte reg, unsigned int len)
+void print_ADC_register(byte reg, int len)
 {
   ADC_data[0] = (1 << 6) + reg;
   
@@ -144,6 +145,16 @@ void set_ADC_ch(unsigned int ch, bool ch_enable)
   ADC_data[2] = 0x10 + (ch << 5);
 
   write_ADC(3);
+
+  // enable/disable printing of the channel data
+  print_enable[ch_to_sens[ch]] = ch_enable;
+}
+
+void disable_all_sensors()
+{
+  // disable all sensors
+  for (int i=0; i<15; i++)
+    set_ADC_ch(i, 0);
 }
 
 void set_ADC_setup()
@@ -163,8 +174,55 @@ void set_ADC_setup()
   write_ADC(3);
 }
 
-void set_ADC_filter()
+void set_ADC_filter(const int ODR)
 {
+  // write to the filter register 0
+  ADC_data[0] = 0x28;
+
+  // set output data rate
+  ADC_data[1] = 0;
+  if (ODR == 125000)
+    ADC_data[2] = B00000;
+  else if (ODR == 62500)
+    ADC_data[2] = B00010;
+  else if (ODR == 31250)
+    ADC_data[2] = B00100;
+  else if (ODR == 25000)
+    ADC_data[2] = B00101;
+  else if (ODR == 15625)
+    ADC_data[2] = B00110;
+  else if (ODR == 10390)
+    ADC_data[2] = B00111;
+  else if (ODR == 4994)
+    ADC_data[2] = B01000;
+  else if (ODR == 2498)
+    ADC_data[2] = B01001;
+  else if (ODR == 1000)
+    ADC_data[2] = B01010;
+  else if (ODR == 500)
+    ADC_data[2] = B01011;
+  else if (ODR == 395)
+    ADC_data[2] = B01100;
+  else if (ODR == 200)
+    ADC_data[2] = B01101;
+  else if (ODR == 100)
+    ADC_data[2] = B01110;
+  else if (ODR == 60)
+    ADC_data[2] = B01111;
+  else if (ODR == 50)
+    ADC_data[2] = B10000;
+  else if (ODR == 20)
+    ADC_data[2] = B10001;
+  else if (ODR == 16)
+    ADC_data[2] = B10010;
+  else if (ODR == 10)
+    ADC_data[2] = B10011;
+  else if (ODR == 5)
+    ADC_data[2] = B10100;
+  else if (ODR == 2)
+    ADC_data[2] = B10101;
+
+  write_ADC(3);
 }
 
 void set_ADC_mode()
@@ -193,31 +251,35 @@ void set_ADC_interface()
 
 void readout_ADC()
 {
+  // check new data is ready
+  digitalWrite(CS_ADC, LOW);
+  delayMicroseconds(1);
+  if (digitalRead(MISO_pin2))
+    return;
+  digitalWrite(CS_ADC, HIGH);
+  
   // read from the data register
   ADC_data[0] = (1 << 6) + 0x04;
   write_ADC(5);
 
-  // determine the channel
-  unsigned int ch = ADC_data[4] & 15;
+  // determine the channel from the status register
+  const unsigned int ch = ADC_data[4] & 15;
 
   // combine three bytes into one variable
-  int val_i = (ADC_data[1] << 16) + (ADC_data[2] << 8) + ADC_data[3];
+  const int val_i = (ADC_data[1] << 16) + (ADC_data[2] << 8) + ADC_data[3];
 
   // store into array for later use
-  readings[ch] = val_i;
+  readings[ch_to_sens[ch]] = val_i - pow(2,23); // 25/pow(2,23) volts
+
+  // indicate data is now ready
+  readings_rdy[ch_to_sens[ch]] = true;
 }
 
-float get_ADC_voltage(const int ch)
+void write_ADC(unsigned int len)
 {
-  // convert into voltage
-  return 25 * (readings[ch]/pow(2,23) - 1);
-}
-
-void print_ADC_data()
-{
-  for (int ch=0; ch<15; ch++) {
-    Serial.print(get_ADC_voltage(ch));
-    Serial.write(", ");
-  }
-  Serial.write("\n");
+  SPI.beginTransaction(SPISettings(100000, MSBFIRST, SPI_MODE0));
+  digitalWrite(CS_ADC, LOW);
+  SPI.transfer(ADC_data, len);
+  digitalWrite(CS_ADC, HIGH);
+  SPI.endTransaction();
 }
