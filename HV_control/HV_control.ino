2226ab21f899c26e6e60474d9b3323b09d8ef1b4
Jakob Kastelic <jakob.kastelic@yale.edu>
2023-02-17 13:40:14 -0600

    updates for HV_control v1.2

--- HV_control/HV_control.ino
+++ HV_control/HV_control.ino
@@ -8,8 +8,9 @@ const int polarity_control = 5;
 const int HV_enable = 6;
 
 // internal state
-byte data[3];
-int next_ch=0, prev_ch=0;
+byte data[4];
+bool printing=false;
+int ch=0;
 long values[5];
 unsigned long previousMillis = 0;
 
@@ -36,15 +37,16 @@ void setup() {
 void loop() {
   check_serial();
 
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= update_interval) {
-    previousMillis = currentMillis;
-    values[prev_ch] = read_ADC(next_ch);
-    prev_ch = next_ch;
-    next_ch = (next_ch + 1) % num_ch;
-
-    if ((next_ch)%num_ch == 0)
-      printout_ADC();
+  if (printing) {
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= update_interval) {
+      previousMillis = currentMillis;
+      values[ch] = read_ADC(ch);
+      ch = (ch + 1) % num_ch;
+  
+      if ((ch%num_ch) == 0)
+        printout_ADC();
+    }
   }
 }
 
@@ -57,39 +59,31 @@ void check_serial()
     // decide what to do with it
     switch (c) {
       case '?':
-        Serial.println("HV_control v1.1 ready.");
+        Serial.println("HV_control v1.2 ready.");
         break;
         
       case 'p':
-        digitalWrite(polarity_control, HIGH);
+        digitalWrite(polarity_control, Serial.parseInt());
         break;
 
-     case 'P':
-        digitalWrite(polarity_control, LOW);
-        break;
-        
       case 'h':
-        digitalWrite(HV_enable, LOW);
+        digitalWrite(HV_enable, Serial.parseInt());
         break;
-        
-     case 'H':
-        digitalWrite(HV_enable, HIGH);
-        break;        
         
       case 'r':
-        digitalWrite(relay_ctl, LOW);
+        digitalWrite(relay_ctl, Serial.parseInt());
         break;
         
-     case 'R':
-        digitalWrite(relay_ctl, HIGH);
+     case 'a':
+        printing = Serial.parseInt();
         break;
 
-     case 'a':
-        printout_ADC();
+     case 'A':
+        read_ADC(Serial.parseInt());
         break;
         
      case 'd':
-        set_DAC(Serial.parseInt()*100);
+        set_DAC(Serial.parseInt());
         break;
     }
   }
@@ -99,36 +93,6 @@ void check_serial()
  * DATA FUNCTIONS
  ***********************************************************/
 
-long read_ADC(int ch)
-{
-  // select channel (see LTC2496 datasheet, pp. 14-15)
-  data[0] = 0b10100011 + ch;
-  write_SPI_ADC();
-
-  // interpret the measured voltage
-  long val = (data[2] >> 4) + (data[1] << 4) + (((long)data[0] & 0b00001111) << 12);
-
-  // interpret the status bits
-  byte status_bits = data[0] >> 4;
-  switch (status_bits) {
-      case 0b0011: // positive out of range
-        val = 99999;
-        break;
-        
-      case 0b0010: // positive
-        break;
-        
-      case 0b0001: // negative
-        val = -val;
-        break;
-
-      case 0b0000: // negative out of range
-        val = -99999;
-        break;
-  }
-
-  return val;
-}
 
 void printout_ADC()
 {
@@ -139,18 +103,30 @@ void printout_ADC()
   Serial.println(values[num_ch-1]);
 }
 
-void set_DAC(int val)
-{
-  data[0] = val >> 8;
-  data[1] = val;
-  write_SPI_DAC();
-}
-
 /***********************************************************
- * CONFIGURATION FUNCTIONS
+ * ADC/DAC FUNCTIONS
  ***********************************************************/
 
+unsigned int read_ADC(int ch) // MAX1300BEUG+
+{
+  // select channel (see MAX1300 datasheet, Table 3)
+  data[0] = 0b10000000 + (ch << 4);
+  data[1] = 0;
+  data[2] = 0;
+  data[3] = 0;
+  write_SPI_ADC();
 
+  // interpret the results
+  return (data[2] << 8) + data[3];
+}
+
+void set_DAC(int val) // MAX5216BGUA+
+{
+  data[0] = (val >> 10) & (0b01111111) | (0b01000000);
+  data[1] = val >> 2;
+  data[2] = val << 6;
+  write_SPI_DAC();
+}
 
 /***********************************************************
  * LOW-LEVEL FUNCTIONS
@@ -163,7 +139,7 @@ void write_SPI_ADC()
   digitalWrite(CS_ADC, LOW);
 
   // send or receive data
-  SPI.transfer(data, 3);
+  SPI.transfer(data, 4);
 
   // end transaction
   digitalWrite(CS_ADC, HIGH);
@@ -173,11 +149,11 @@ void write_SPI_ADC()
 void write_SPI_DAC()
 {
   // begin transaction
-  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE1));
   digitalWrite(CS_DAC, LOW);
 
   // send or receive data
-  SPI.transfer(data, 2);
+  SPI.transfer(data, 3);
 
   // end transaction
   digitalWrite(CS_DAC, HIGH);
